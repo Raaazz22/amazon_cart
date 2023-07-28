b0a40a324deec5d9f234ed0028e97685ed30b3e8
Raaazz22 <128067972+Raaazz22@users.noreply.github.com>
2023-07-28 20:28:53 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//alert("hii")
+
+function output()
+{
+	a = document.getElementById("select").value;
+    document.getElementById("it").innerHTML=a;
+	document.getElementById("it2").innerHTML=a*1099.00;
+	document.getElementById("it3").innerHTML=a;
+	document.getElementById("it4").innerHTML=a*1099.00;
+}
